952223ad446146d800f6f5ba19e22e5531f02ccf
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-25 10:05:00 +0100

    yty

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #Using Puppet, install flask from pip3.
+<<<<<<< HEAD
 package { ['flask', 'werkzeug']:
+=======
+package { 'flask':
+>>>>>>> 490ab67 (yty)
   ensure   => '2.1.0',
   provider => 'pip',
   command  => '/usr/bin/pip3', 
