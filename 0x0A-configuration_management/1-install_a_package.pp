44cabcfcbad1946cc179147fa1331ba09b5a46ec
abdelouahed <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-02-25 09:41:43 +0100

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #Using Puppet, install flask from pip3.
-package { 'flask':
+package { ['flask', 'werkzeug']:
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
   command  => '/usr/bin/pip3', 
   require  => Package['python3-pip'], 
-}
+
