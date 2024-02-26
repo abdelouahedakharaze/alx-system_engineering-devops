a1bd6ba359874044eeb7f780b88afe21c995876a
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-26 12:41:36 +0100

    r

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# Using Puppet, install flask from pip3
 
-package { 'flask':
+#!/usr/bin/pup
+
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
+}
+
+# Install an especific version of Werkzeug (2.1.1)
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
